44c42e17077cab99b685bec33600d71cad1f0f47
hichem_lazili <hichem@aivot.agency>
2023-09-08 16:29:58 +0100

    initial commit

---
+++ components/OTable.vue
@@ -0,0 +1,69 @@
+<template>
+ <div>
+  <v-data-table
+    :headers="headers"
+    :items="rows"
+    item-value="name"
+    class="elevation-1"
+  >
+    <template v-slot:item.actions="{ item }">
+      <div class="d-flex align-center justify-center">
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          x-small
+          color="primary"
+        >
+          <v-icon small>
+            mdi-pencil
+          </v-icon>
+        </v-btn>
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          x-small
+          color="red"
+        >
+          <v-icon small>
+            mdi-delete
+          </v-icon>
+        </v-btn>
+      </div>
+    </template>
+  </v-data-table>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "OTable",
+  props: {
+    withActions: {
+      default: false,
+      required: false,
+      type: Boolean
+    },
+    rows: {
+      required: true,
+      type: Array,
+    },
+    columns: {
+      required: true,
+      type: Array,
+    }
+  },
+  computed: {
+    headers(){
+      if(this.withActions)
+        return this.columns.concat({text: 'Actions', value: 'actions', align: 'center'})
+      return this.columns
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="d-flex flex-column gap-4">
+    <div class="d-flex justify-space-between align-center py-10">
+      <h3 class="text-md-h3 text--primary">
+        Liste des graphs
+      </h3>
+
+      <v-btn
+        prepend-icon="mdi-plus"
+        color="primary"
+      >
+        Ajouter
+      </v-btn>
+    </div>
+
+    <o-table :rows="graphs" :columns="headers" with-actions />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data: () => ({
+    graphs: [
+      {
+        id: 1,
+        name: 'first graph',
+        description: 'my awesome graph description',
+      }
+    ],
+    headers: [
+      {
+        text: 'Id',
+        align: 'start',
+        value: 'id',
+        sortable: false,
+      },
+      { text: 'Name', value: 'name' },
+      { text: 'Description', value: 'description' },
+    ],
+  }),
+}
+</script>
